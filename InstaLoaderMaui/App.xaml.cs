57fc595db10a38fb1bbab20972930160e3dc094f
mvxGREEN <mvxgreen@gmail.com>
2025-06-01 03:28:47 -0500

    fixed UI (was missing all my shadow hexes)

--- InstaLoaderMaui/App.xaml.cs
+++ InstaLoaderMaui/App.xaml.cs
@@ -1,12 +1,21 @@
-﻿namespace InstaLoaderMaui
+﻿using Android.Nfc;
+
+namespace InstaLoaderMaui
 {
     public partial class App : Application
     {
+        private static readonly string Tag = nameof(App);
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            Console.WriteLine($"{Tag}: CreateWindow");
+            //return new Window(new AppShell());
+            return base.CreateWindow(activationState);
         }
     }
 }
